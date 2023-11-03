e2f53781af86f1bea80ccb8b9a799b713e35c592
onepiece010938 <onepiece010938@gmail.com>
2023-11-03 15:52:24 +0800

    project init

--- internal/adapter/cache/message_cache.go
+++
@@ -1,8 +0,0 @@
-package cache
-
-const message_prefix = "message:"
-
-func (c *Cache) GetMessageCache(input string) string {
-	// c.cache.Set()
-	return message_prefix + "123"
-}
--- internal/domain/message/message.go
+++
@@ -1,9 +0,0 @@
-package message
-
-// func MessageDomainFunc(string) MessageDomainResult {}
-
-type MessageDomainResult struct {
-}
-
-// func update()
-// func Get ()
